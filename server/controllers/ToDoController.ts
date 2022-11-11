43c1985a35907660e28e088c64ab0f4d82b95298
Guy <somesome@some.com>
2022-11-11 23:08:14 +0200

    fix imports

--- server/controllers/ToDoController.ts
+++ server/controllers/ToDoController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express';
-import countDocuments, { create, find, findByIdAndDelete, findByIdAndUpdate } from '../models/ToDoModel';
+import todo from '../models/ToDoModel';
 
 export async function getToDo(req: Request, res: Response) {
   const { page, limit } = req.query;
   if (limit && page)
     try {
-      const todos = await find()
+      const todos = await todo
+        .find()
         .limit(+limit * 1)
         .skip((+page - 1) * +limit)
         .exec();
-      const count = await countDocuments();
+      const count = await todo.countDocuments();
 
       res.set(200).send({
         todos,
@@ -26,7 +27,7 @@ export async function getToDo(req: Request, res: Response) {
 export async function saveToDo(req: Request, res: Response) {
   try {
     const { desc } = req.body;
-    const newModel = await create({ desc });
+    const newModel = await todo.create({ desc });
     console.log('Added');
     console.log(newModel);
     res.set(201).send(newModel);
@@ -40,7 +41,7 @@ export async function deleteToDo(req: Request, res: Response) {
   try {
     const { _id } = req.body;
     console.log(_id);
-    const a = await findByIdAndDelete(_id);
+    const a = await todo.findByIdAndDelete(_id);
     console.log(a);
     res.set(200).send('deleted');
   } catch (err) {
@@ -52,7 +53,7 @@ export async function deleteToDo(req: Request, res: Response) {
 export async function updateToDo(req: Request, res: Response) {
   try {
     const { _id, done } = req.body;
-    await findByIdAndUpdate(_id, { done });
+    await todo.findByIdAndUpdate(_id, { done });
     res.set(200).send('Updated');
   } catch (err) {
     console.log(err);

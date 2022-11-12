4b01e6dbddfbf705a9c481ca43c079705e17620b
ofirlevy6658 <ofirlevy6658@gmail.com>
2022-11-12 13:13:35 +0200

    add login page

--- client/vite-todo/src/App.tsx
+++ client/vite-todo/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Todo } from "./pages/Todo";
+import { LoginPage } from "./pages/LoginPage";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Todo />
+      <LoginPage />
+      {/* <Todo /> */}
     </QueryClientProvider>
   );
 }
